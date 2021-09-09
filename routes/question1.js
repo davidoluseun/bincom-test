5aa81f2964a934fb6f841b77cad9759f9cfc1461
David Oluseun <a.davidoluseun@gmail.com>
2021-09-09 09:45:16 +0100

    Add not-found message

--- routes/question1.js
+++ routes/question1.js
@@ -4,15 +4,14 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   const pollingUnitResults = data.announced_pu_results;
-
   const pollingUnitUniqueID = "9";
 
   const results = pollingUnitResults.filter(
     (p) => p.polling_unit_uniqueid === pollingUnitUniqueID
   );
+  const { length: count } = results;
 
-  res.render("index", { results, pollingUnitUniqueID });
+  res.render("index", { results, pollingUnitUniqueID, count });
 });
 
 module.exports = router;
-
