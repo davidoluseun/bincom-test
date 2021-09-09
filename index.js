743b511e80ad9369db55a5cb6ea50965ca3a2583
David Oluseun <a.davidoluseun@gmail.com>
2021-09-09 06:33:21 +0100

    Add Question 3 solution

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { data } = require("./bincom_test_db");
 app.set("view engine", "ejs");
 
 const pollingUnitResults = data.announced_pu_results;
+const pollingUnits = data.polling_unit;
 
 app.get("/", (req, res) => {
   const pollingUnitUniqueID = "8";
@@ -19,7 +20,6 @@ app.get("/", (req, res) => {
 app.get("/question2", (req, res) => {
   const lga = data.lga;
   const lgaId = req.query.lga ? req.query.lga : 0;
-  const pollingUnits = data.polling_unit;
 
   const lgaPollingUnits = pollingUnits.filter((p) => p.lga_id === lgaId);
   let lgaPollingUnitResults = [];
@@ -40,7 +40,35 @@ app.get("/question2", (req, res) => {
 });
 
 app.get("/question3", (req, res) => {
-  res.render("question3");
+  const parties = data.party;
+
+  const query = req.query;
+  const fullname = req.query.fullname;
+  const date_entered = new Date();
+  delete query.fullname;
+
+  const lastUniqueId = parseInt(pollingUnits[pollingUnits.length - 1].uniqueid);
+  const lastResultId = parseInt(
+    pollingUnitResults[pollingUnitResults.length - 1].result_id
+  );
+  const newUniqueId = (lastUniqueId + 1).toString();
+  let newResultId = (lastResultId + 1).toString();
+
+  for (field in query) {
+    const newPollingUnitResult = {
+      result_id: newResultId++,
+      polling_unit_uniqueid: newUniqueId,
+      party_abbreviation: field,
+      party_score: query[field],
+      entered_by_user: fullname,
+      date_entered,
+      user_ip_address: "192.168.1.101",
+    };
+
+    pollingUnitResults.push(newPollingUnitResult);
+  }
+
+  res.render("question3", { parties });
 });
 
 const port = process.env.PORT || 5000;
